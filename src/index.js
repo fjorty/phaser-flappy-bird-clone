841dbae4a81c5c763a36c69298663dc4f5132b17
fjorty <naz.lavryk@gmail.com>
2022-06-13 22:22:21 +0300

    Added base layout and logic

--- src/index.js
+++ src/index.js
@@ -1,28 +1,80 @@
-
-import Phaser from "phaser";
+import Phaser from 'phaser';
 
 const config = {
   type: Phaser.AUTO,
   width: 800,
   height: 600,
+  pixelArt: true,
   physics: {
     default: 'arcade',
     arcade: {
-      gravity: { y: 200 }
+      debug: true,
+      gravity: {
+        y: 0
+      }
     }
   },
   scene: {
-    preload: preload,
-    create: create
+    preload,
+    create,
+    update
   }
 };
 
-new Phaser.Game(config);
+const INITIAL_BIRD_POSITION = {
+  x: config.width / 10,
+  y: config.height / 2
+};
+const BIRD_GRAVITY = 300;
+const FLAP_VELOCITY = 250;
+const PIPE_VERTICAL_DISTANCE_RANGE = [150, 250];
+const PIPE_HORIZONTAL_DISTANCE_RANGE = [380, 430];
 
-function preload () {
+const PIPES_TO_RENDER = 4;
+
+let bird = null;
+let upperPipe = null;
+let lowerPipe = null;
+let pipeHorizontalDistance = 0;
+
+function preload() {
   this.load.image('sky', 'assets/sky.png');
+  this.load.image('bird', 'assets/bird.png');
+  this.load.image('pipe', 'assets/pipe.png');
 }
 
-function create () {
-  this.add.image(400, 300, 'sky');
+function create() {
+  this.add.image(0, 0, 'sky').setOrigin(0);
+
+  bird = this.physics.add.sprite(INITIAL_BIRD_POSITION.x, INITIAL_BIRD_POSITION.y, 'bird').setOrigin(0);
+  bird.body.gravity.y = BIRD_GRAVITY;
+
+  this.input.on('pointerdown', flap);
+  this.input.keyboard.on('keydown_SPACE', flap);  
+
+  for (let i = 0; i < PIPES_TO_RENDER; i++) {
+    pipeHorizontalDistance += Phaser.Math.Between(...PIPE_HORIZONTAL_DISTANCE_RANGE);
+    const pipeVerticalDistance = Phaser.Math.Between(...PIPE_VERTICAL_DISTANCE_RANGE);
+    const pipeVerticalPosition = Phaser.Math.Between(0 + 20, config.height - 20 - pipeVerticalDistance);
+    upperPipe = this.physics.add.sprite(pipeHorizontalDistance, pipeVerticalPosition, 'pipe').setOrigin(0, 1);
+    lowerPipe = this.physics.add.sprite(upperPipe.x, upperPipe.y + pipeVerticalDistance, 'pipe').setOrigin(0);
+  }
 }
+
+function update(time, delta) {
+  if (bird.body.position.y >= (config.height - bird.body.height) || bird.body.position.y <= 0) {
+    restartBirdPosition();
+  }
+}
+
+function flap() {
+  bird.body.velocity.y = -FLAP_VELOCITY;
+}
+
+function restartBirdPosition() {
+  bird.body.position.x = INITIAL_BIRD_POSITION.x;
+  bird.body.position.y = INITIAL_BIRD_POSITION.y;
+  bird.body.velocity.y = 0;
+}
+
+new Phaser.Game(config);
